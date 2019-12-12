751069211a3cfa57a620ca375e6583dc14d06c81
mortalliao <mortalliaoyujian@163.com>
2019-12-12 22:55:10 +0800

    spring-cloud eureka

--- easycloud/backend/easycloud-demo/easycloud-jwt/src/main/java/win/mortalliao/jwt/config/mybatisplus/base/service/SuperService.java
+++
@@ -1,9 +0,0 @@
-package win.mortalliao.jwt.config.mybatisplus.base.service;
-
-import com.baomidou.mybatisplus.service.IService;
-
-/**
- * @author mortalliao
- */
-public interface SuperService<T> extends IService<T> {
-}
--- easycloud/backend/easycloud-demo/easycloud-weixin/src/main/java/win/mortalliao/weixin/handler/ScanHandler.java
+++
@@ -1,8 +0,0 @@
-package win.mortalliao.weixin.handler;
-
-/**
- * @author Binary Wang(https://github.com/binarywang)
- */
-public abstract class ScanHandler extends AbstractHandler {
-
-}
